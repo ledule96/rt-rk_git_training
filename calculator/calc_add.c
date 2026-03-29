44b7fb533fac5946e2ce7200e9d22f81bdbb7504
dpopovic <dusko.popovic@tttech.com>
2026-03-29 14:45:49 +0200

    RTRK-2: Implemented add operation.

---
+++ calculator/calc_add.c
@@ -0,0 +1,8 @@
+#include "calc_add.h"
+
+#include "stdio.h"
+
+void calc_add(int x, int y)
+{
+	printf("Result = %d\r\n", x + y);
+}
---
+++ calculator/calc_add.h
@@ -0,0 +1,6 @@
+#ifndef CALC_ADD_H
+#define CALC_ADD_H
+
+void calc_add(int x, int y);
+
+#endif
