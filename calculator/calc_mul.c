223ced2cf58a852fc5330a5b35dde303d7c73f0c
dpopovic <dusko.popovic@tttech.com>
2026-03-31 08:36:16 +0200

    RTRK-3: Implement multiply routine.

---
+++ calculator/calc_mul.c
@@ -0,0 +1,8 @@
+#include "calc_mul.h"
+
+#include "stdio.h"
+
+void calc_mul(int x, int y)
+{
+	printf("Result = %d\r\n", x * y);
+}
---
+++ calculator/calc_mul.h
@@ -0,0 +1,6 @@
+#ifndef CALC_MUL_H
+#define CALC_MUL_H
+
+void calc_mul(int x, int y);
+
+#endif
